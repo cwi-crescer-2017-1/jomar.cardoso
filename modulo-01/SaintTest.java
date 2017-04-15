8469143bb73311d091c65d9880b31bed2fde7b7c
Jomar <jomar.cardoso@cwi.com.br>
2017-04-15 17:04:51 -0300

    test(Saint) criado teste para tres possibilidades de iniciar batalha

--- modulo-01/SaintTest.java
+++ modulo-01/SaintTest.java
@@ -44,4 +44,36 @@ public class SaintTest
      double vidaAtual = roxetta.perderVida(14);
      assertEquals(86.0, vidaAtual, 0.001);
  }
+ 
+  
+ @Test
+ public void testarRetornoDaCategoriaArmadura() {
+     boolean testa = true;
+      Saint mana = new Saint("Mana", new Armadura("Cisne", Categoria.OURO));
+      
+     assertEquals(true, testa);
+ }
+ 
+ @Test
+  public void testarPrimeiroAtaqueEVerificarSePerdeuVida(){
+      /// AAA
+      // 1. Arrange - Montagem dos dados de teste
+      Batalha batalha1 = new Batalha();
+      Batalha batalha2 = new Batalha();
+      Batalha batalha3 = new Batalha();
+      Armadura escorpiao = new Armadura("Escorpiao", Categoria.PRATA);
+      Armadura abutre = new Armadura("Abutre", Categoria.OURO);
+      Saint jomar = new Saint("Jomar", escorpiao);      
+      Saint zeca = new Saint("Zeca", abutre);
+      Saint joca = new Saint("Joca", escorpiao);
+      // 2. Act - Invocar a ação a ser testada
+      batalha1.iniciar(jomar, zeca);
+      batalha2.iniciar(joca, jomar);
+      batalha3.iniciar(zeca, joca);
+      // 3. Assert - Verificação dos resultados do teste
+      jomar.getVida();
+      assertEquals(jomar.getVida(), 80, 0.001);
+      assertEquals(zeca.getVida(), 100, 0.001);
+      assertEquals(joca.getVida(), 90, 0.001);
+  }
 }
