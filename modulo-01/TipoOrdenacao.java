0268b6803cc52c8436860278f623d4dc771e8271
Jomar <jomar.cardoso@cwi.com.br>
2017-04-19 17:22:47 -0300

    feat(TipoOrdenacao): ordenar ascendente e descendente

---
+++ modulo-01/TipoOrdenacao.java
@@ -0,0 +1,16 @@
+
+/**
+Descendente
+
+Crie uma enumeração chamada TipoOrdenacao que recebe dois valores: ASCENDENTE e DESCENDENTE e adicione um outro método também chamado
+ ordenar na classe ListaSaints mas que recebe um parâmetro: TipoOrdenacao. 
+ Ou seja, a classe deve ficar com (além dos outros métodos que foram implementados previamente):
+
+ordenar() => Já implementado anteriormente
+ordenar(TipoOrdenacao) => Método a ser feito neste exercício.
+ */
+public enum TipoOrdenacao
+{
+    ASCENDENTE,
+    DESCENDENTE;
+}
