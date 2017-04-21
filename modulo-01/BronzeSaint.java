ff3beac9a858e0c8dc375d18ee36ddab97d0b070
Jomar <jomar.cardoso@cwi.com.br>
2017-04-20 23:40:19 -0300

    feat(Movimento): interface movimento e classe vestir armadura

--- modulo-01/BronzeSaint.java
+++ modulo-01/BronzeSaint.java
@@ -1,7 +1,7 @@
 public class BronzeSaint extends Saint {
     
     public BronzeSaint(String nome, String constelacao) throws Exception {
-        this(nome, new Armadura(new Constelacao(constelacao), Categoria.BRONZE));
+        this(nome, new Armadura(new Constelacao(constelacao), Categoria.BRONZE));        
     }
     
     public BronzeSaint(String nome, Armadura armadura) throws Exception {
