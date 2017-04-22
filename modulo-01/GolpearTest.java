486de35c1916a420eae40b427c5313b1185e870d
Jomar <jomar.cardoso@cwi.com.br>
2017-04-21 23:46:57 -0300

    fix(GolpearTest): valores

--- modulo-01/GolpearTest.java
+++ modulo-01/GolpearTest.java
@@ -18,27 +18,30 @@ public class GolpearTest
        Saint seya = new BronzeSaint("Seya", "Pégaso");
        Saint marin = new SilverSaint("Marin", "Águia");
        Saint mu = new GoldSaint("Mu", "Áries");
+       Golpe soco = new Golpe("Soco", 8);
+       seya.aprenderGolpe(soco);
+       marin.aprenderGolpe(soco);
+       mu.aprenderGolpe(soco);       
        
        Golpear golpe1 = new Golpear(seya, marin);
        golpe1.executar();
-       assertEquals(98.0, marin.getVida(), 0.01);
+       assertEquals(92.0, marin.getVida(), 0.01);
        seya.vestirArmadura();
        golpe1.executar();
-       assertEquals(94.0, marin.getVida(), 0.01);
+       assertEquals(76.0, marin.getVida(), 0.01);
        
        Golpear golpe2 = new Golpear(marin, mu);
        golpe2.executar();
-       assertEquals(98.0, mu.getVida(), 0.01);
+       assertEquals(92.0, mu.getVida(), 0.01);
        marin.vestirArmadura();
        golpe2.executar();
-       assertEquals(92.0, mu.getVida(), 0.01);
+       assertEquals(68.0, mu.getVida(), 0.01);
        
        Golpear golpe3 = new Golpear(mu, seya);
        golpe3.executar();
-       assertEquals(98.0, seya.getVida(), 0.01);
+       assertEquals(92.0, seya.getVida(), 0.01);
        mu.vestirArmadura();
        golpe3.executar();
-       assertEquals(90.0, seya.getVida(), 0.01);
-       
+       assertEquals(60.0, seya.getVida(), 0.01);     
     }
 }
