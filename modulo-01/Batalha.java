c288d82277c60b0c8afbe44aa54908de08d073dc
Jomar <jomar.cardoso@cwi.com.br>
2017-04-22 01:48:40 -0300

    feat(Batalha): pelear até a morte

--- modulo-01/Batalha.java
+++ modulo-01/Batalha.java
@@ -8,15 +8,26 @@ public class Batalha {
     }
 
     public void iniciar() {
-        int valor1 = this.saint1.getArmadura().getCategoria().getValor();
-        int valor2 = this.saint2.getArmadura().getCategoria().getValor();
-        final double dano = 10;
-
-        if (valor1 >= valor2) {
-            this.saint2.perderVida(dano);
-        } else {
-            this.saint1.perderVida(dano);
-        }
+       int valor1 = this.saint1.getArmadura().getCategoria().getValor();
+       int valor2 = this.saint2.getArmadura().getCategoria().getValor();
+       boolean vivo = true;
+       Saint atacante, atacado, aux;
+       if (valor1 >= valor2) {
+           atacante = this.saint1;
+           atacado = this.saint2;  
+       } else {
+           atacante = this.saint2;
+           atacado = this.saint1;
+       }
+       
+       do {
+           Golpear golpe = new Golpear(atacante, atacado);
+           golpe.executar();
+           vivo = Status.VIVO.equals(atacado.getStatus());
+           aux = atacante;
+           atacante = atacado;
+           atacado = aux;
+       } while(vivo);
 
     }
 }
--- modulo-01/BatalhaTest.java
+++ modulo-01/BatalhaTest.java
@@ -3,43 +3,17 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class BatalhaTest {
+public class BatalhaTest {    
     @Test
-    public void categoriaSaint1MaiorQueSaint2() throws Exception {
-        // Arrange
-        Saint shaina = new SilverSaint("Shaina", new Armadura(new Constelacao("Serpente"), Categoria.PRATA));
-        Saint hyoga = new BronzeSaint("Hyoga", new Armadura(new Constelacao("Cisne"), Categoria.BRONZE));
-        Batalha batalha = new Batalha(shaina, hyoga);
-        // Act
-        batalha.iniciar();
-        // Assert
-        assertEquals(100, shaina.getVida(), 0.01);
-        assertEquals(90, hyoga.getVida(), 0.01);
-    }
-    
-    @Test
-    public void categoriasIguaisSaint2PerdeVida() throws Exception {
-        // Arrange
-        Saint aldebaran = new GoldSaint("Aldebaran", new Armadura(new Constelacao("Touro"), Categoria.OURO));
-        Saint mascaraMorte = new GoldSaint("Máscara da Morte", new Armadura(new Constelacao("Câncer"), Categoria.OURO));
-        Batalha batalha = new Batalha(aldebaran, mascaraMorte);
-        // Act
-        batalha.iniciar();
-        // Assert
-        assertEquals(100, aldebaran.getVida(), 0.01);
-        assertEquals(90, mascaraMorte.getVida(), 0.01);
-    }
-    
-    @Test
-    public void categoriaSaint2MaiorSaint1PerdeVida() throws Exception {
-        // Arrange
-        Saint ikki = new BronzeSaint("Ikki", new Armadura(new Constelacao("Fênix"), Categoria.BRONZE));
-        Saint mascaraMorte = new GoldSaint("Máscara da Morte", new Armadura(new Constelacao("Câncer"), Categoria.OURO));
-        Batalha batalha = new Batalha(ikki, mascaraMorte);
-        // Act
-        batalha.iniciar();
-        // Assert
-        assertEquals(90, ikki.getVida(), 0.01);
-        assertEquals(100, mascaraMorte.getVida(), 0.01);
+    public void pelearAteMorrer() throws Exception{
+       Saint seya = new BronzeSaint("Seya", "Pégaso");
+       Saint marin = new SilverSaint("Marin", "Águia");
+       Golpe soco = new Golpe("Soco", 8);
+       seya.aprenderGolpe(soco);
+       marin.aprenderGolpe(soco);
+       Batalha batalha1 = new Batalha(seya, marin);
+       batalha1.iniciar();
+       assertEquals(Status.VIVO, marin.getStatus());
+       assertEquals(Status.MORTO, seya.getStatus());
     }
 }
