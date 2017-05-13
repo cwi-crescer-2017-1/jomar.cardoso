2e664b4ec4fd3539836ea88b4100820ff6108285
Jomar <jomar.cardoso@cwi.com.br>
2017-05-13 06:56:22 -0300

    feat(modulo4/aula2/atividade2): exercicios 6,7,8 (#NUNESILLUMINATI)

--- modulo-04/aula2/js/js.js
+++ modulo-04/aula2/js/js.js
@@ -1,4 +1,4 @@
-var series = JSON.parse('[{"titulo":"Stranger Things","anoEstreia":2016,"diretor":["Matt Duffer","Ross Duffer"],"genero":["Suspense","Ficcao Cientifica","Drama"],"elenco":["Winona Ryder","David Harbour","Finn Wolfhard","Millie Bobby Brown","Gaten Matarazzo","Caleb McLaughlin","Natalia Dyer","Charlie Heaton","Cara Buono","Matthew Modine","Noah Schnapp"],"temporadas":1,"numeroEpisodios":8,"distribuidora":"Netflix"},{"titulo":"Game Of Thrones","anoEstreia":2011,"diretor":["David Benioff","D. B. Weiss","Carolyn Strauss","Frank Doelger","Bernadette Caulfield","George R. R. Martin"],"genero":["Fantasia","Drama"],"elenco":["Peter Dinklage","Nikolaj Coster-Waldau","Lena Headey","Emilia Clarke","Kit Harington","Aidan Gillen","Iain Glen  ","Sophie Turner","Maisie Williams","Alfie Allen","Isaac Hempstead Wright"],"temporadas":6,"numeroEpisodios":60,"distribuidora":"HBO"},{"titulo":"The Walking Dead","anoEstreia":2010,"diretor":["Jolly Dale","Caleb Womble","Paul Gadd","Heather Bellson"],"genero":["Terror","Suspense","Apocalipse Zumbi"],"elenco":["Andrew Lincoln","Jon Bernthal","Sarah Wayne Callies","Laurie Holden","Jeffrey DeMunn","Steven Yeun","Chandler Riggs ","Norman Reedus","Lauren Cohan","Danai Gurira","Michael Rooker ","David Morrissey"],"temporadas":7,"numeroEpisodios":99,"distribuidora":"AMC"},{"titulo":"Band of Brothers","anoEstreia":20001,"diretor":["Steven Spielberg","Tom Hanks","Preston Smith","Erik Jendresen","Stephen E. Ambrose"],"genero":["Guerra"],"elenco":["Damian Lewis","Donnie Wahlberg","Ron Livingston","Matthew Settle","Neal McDonough"],"temporadas":1,"numeroEpisodios":10,"distribuidora":"HBO"},{"titulo":"Bernardo The Master of the Wizards","anoEstreia":2017,"diretor":["James Bajczuk","Marcio Tesser","Andre Nunes"],"genero":["Terror","Caos","JavaScript"],"elenco":["Alana Weiss","Alexia Pereira","Bruno Aguiar","Camila Batista","Christopher Michel","Claudia Moura","Deordines Tomazi","Diandra Rocha","Jabel Fontoura","João Silva","Jomar Cardoso","Leonardo Alves","Leonardo Morais","Lucas Damaceno","Lucas Gaspar","Lucas Muller","Luis Robinson","Maico Kley","Mateus Silva","Mathias Ody","Mirela Adam","Rafael Barizon","Rafael Barreto","Tais Silva","William Goncalves"],"temporadas":1,"numeroEpisodios":40,"distribuidora":"CWI"},{"titulo":"10 Days Why","anoEstreia":2010,"diretor":["Brendan Eich"],"genero":["Caos","JavaScript"],"elenco":["Brendan Eich","Bernardo Bosak"],"temporadas":10,"numeroEpisodios":10,"distribuidora":"JS"},{"titulo":"Mr. Robot","anoEstreia":2018,"diretor":["Sam Esmail"],"genero":["Drama","Techno Thriller","Psychological Thriller"],"elenco":["Rami Malek","Carly Chaikin","Portia Doubleday","Martin Wallström","Christian Slater"],"temporadas":2,"numeroEpisodios":22,"distribuidora":"USA Network"},{"titulo":"Narcos","anoEstreia":2015,"diretor":["Paul Eckstein","Mariano Carranco","Tim King","Lorenzo O Brien"],"genero":["Documentario","Crime","Drama"],"elenco":["Wagner Moura","Boyd   Holbrook","Pedro Pascal","Joann Christie","Mauricie Compte","André Mattos","Roberto Urbina","Diego Cataño","Jorge A. Jiménez","Paulina Gaitán","Paulina Garcia"],"temporadas":2,"numeroEpisodios":20,"distribuidora": null},{"titulo":"Westworld","anoEstreia":2016,"diretor":["Athena Wickham"],"genero":["Ficcao Cientifica","Drama","Thriller","Acao","Aventura","Faroeste"],"elenco":["Evan N. Rachel Wood","Thandie U. Newton","Jeffrey N. Wright","James E. Marsden","Ben S. Barnes","Ingrid I. Bolso Berdal","Clifton L. Collins Jr.","Luke L. Hemsworth","Sidse U. Babett Knudsen","Simon M. Quarterman","Rodrigo I. Santoro","Tessa N. Thompson","Shannon A. Woodward","Ed T. Harris","Anthony I. Hopkins"],"temporadas":1,"numeroEpisodios":10,"distribuidora":"HBO"},{"titulo":"Breaking Bad","anoEstreia":2008,"diretor":["Vince Gilligan","Michelle MacLaren","Adam Bernstein","Colin Bucksey","Michael Slovis","Peter Gould"],"genero":["Acao","Suspense","Drama","Crime","Humor Negro"],"elenco":["Bryan Cranston","Anna Gunn","Aaron Paul","Dean Norris","Betsy Brandt","RJ Mitte"],"temporadas":5,"numeroEpisodios":62,"distribuidora":"AMC"}]');
+var series = JSON.parse('[{"titulo":"Stranger Things","anoEstreia":2016,"diretor":["Matt Duffer","Ross Duffer"],"genero":["Suspense","Ficcao Cientifica","Drama"],"elenco":["Winona Ryder","David Harbour","Finn Wolfhard","Millie Bobby Brown","Gaten Matarazzo","Caleb McLaughlin","Natalia Dyer","Charlie Heaton","Cara Buono","Matthew Modine","Noah Schnapp"],"temporadas":1,"numeroEpisodios":8,"distribuidora":"Netflix"},{"titulo":"Game Of Thrones","anoEstreia":2011,"diretor":["David Benioff","D. B. Weiss","Carolyn Strauss","Frank Doelger","Bernadette Caulfield","George R. R. Martin"],"genero":["Fantasia","Drama"],"elenco":["Peter Dinklage","Nikolaj Coster-Waldau","Lena Headey","Emilia Clarke","Kit Harington","Aidan Gillen","Iain Glen  ","Sophie Turner","Maisie Williams","Alfie Allen","Isaac Hempstead Wright"],"temporadas":6,"numeroEpisodios":60,"distribuidora":"HBO"},{"titulo":"The Walking Dead","anoEstreia":2010,"diretor":["Jolly Dale","Caleb Womble","Paul Gadd","Heather Bellson"],"genero":["Terror","Suspense","Apocalipse Zumbi"],"elenco":["Andrew Lincoln","Jon Bernthal","Sarah Wayne Callies","Laurie Holden","Jeffrey DeMunn","Steven Yeun","Chandler Riggs ","Norman Reedus","Lauren Cohan","Danai Gurira","Michael Rooker ","David Morrissey"],"temporadas":7,"numeroEpisodios":99,"distribuidora":"AMC"},{"titulo":"Band of Brothers","anoEstreia":20001,"diretor":["Steven Spielberg","Tom Hanks","Preston Smith","Erik Jendresen","Stephen E. Ambrose"],"genero":["Guerra"],"elenco":["Damian Lewis","Donnie Wahlberg","Ron Livingston","Matthew Settle","Neal McDonough"],"temporadas":1,"numeroEpisodios":10,"distribuidora":"HBO"},{"titulo":"Bernardo The Master of the Wizards","anoEstreia":2017,"diretor":["James Bajczuk","Marcio Tesser","Andre Nunes"],"genero":["Terror","Caos","JavaScript"],"elenco":["Alana Weiss","Alexia Pereira","Bruno Aguiar","Camila Batista","Christopher Michel","Claudia Moura","Deordines Tomazi","Diandra Rocha","Jabel Fontoura","João Silva","Jomar Cardoso","Leonardo Alves","Leonardo Morais","Lucas Damaceno","Lucas Gaspar","Lucas Muller","Luis Robinson","Maico Kley","Mateus Silva","Mathias Ody","Mirela Adam","Rafael Barizon","Rafael Barreto","Tais Silva","William Goncalves"],"temporadas":1,"numeroEpisodios":40,"distribuidora":"CWI"},{"titulo":"10 Days Why","anoEstreia":2010,"diretor":["Brendan Eich"],"genero":["Caos","JavaScript"],"elenco":["Brendan Eich","Bernardo Bosak"],"temporadas":10,"numeroEpisodios":10,"distribuidora":"JS"},{"titulo":"Mr. Robot","anoEstreia":2018,"diretor":["Sam Esmail"],"genero":["Drama","Techno Thriller","Psychological Thriller"],"elenco":["Rami Malek","Carly Chaikin","Portia Doubleday","Martin Wallström","Christian Slater"],"temporadas":2,"numeroEpisodios":22,"distribuidora":"USA Network"},{"titulo":"Narcos","anoEstreia":2015,"diretor":["Paul Eckstein","Mariano Carranco","Tim King","Lorenzo O Brien"],"genero":["Documentario","Crime","Drama"],"elenco":["Wagner Moura","Boyd   Holbrook","Pedro Pascal","Joann Christie","Mauricie Compte","André Mattos","Roberto Urbina","Diego Cataño","Jorge A. Jiménez","Paulina Gaitán","Paulina Garcia"],"temporadas":2,"numeroEpisodios":20,"distribuidora": null},{"titulo":"Westworld","anoEstreia":2016,"diretor":["Athena Wickham"],"genero":["Ficcao Cientifica","Drama","Thriller","Acao","Aventura","Faroeste"],"elenco":["Evan N. Rachel Wood","Thandie U. Newton","Jeffrey N. Wright","James E. Marsden","Ben S. Barnes","Ingrid I. Bolso Berdal","Clifton L. Collins","Luke L. Hemsworth","Sidse U. Babett Knudsen","Simon M. Quarterman","Rodrigo I. Santoro","Tessa N. Thompson","Shannon A. Woodward","Ed T. Harris","Anthony I. Hopkins"],"temporadas":1,"numeroEpisodios":10,"distribuidora":"HBO"},{"titulo":"Breaking Bad","anoEstreia":2008,"diretor":["Vince Gilligan","Michelle MacLaren","Adam Bernstein","Colin Bucksey","Michael Slovis","Peter Gould"],"genero":["Acao","Suspense","Drama","Crime","Humor Negro"],"elenco":["Bryan Cranston","Anna Gunn","Aaron Paul","Dean Norris","Betsy Brandt","RJ Mitte"],"temporadas":5,"numeroEpisodios":62,"distribuidora":"AMC"}]');
 console.log(series);
 
 /*
@@ -47,7 +47,7 @@ function filtrarSeriesPorAno(series, ano) {
   })
   return retorno
 }
-console.log(filtrarSeriesPorAno(series, 2017))
+console.log('series antes depois de 2017',filtrarSeriesPorAno(series, 2017))
 
 /*
 Exercício 3: Média de Episódios
@@ -130,45 +130,143 @@ Vamos começar a dificultar um pouco mais.
 */
 
 console.log("===EXERCICIO 6===")
+function queroGenero(series, gene){
+  var retorno = []
+  series.forEach(function(serie){
+    for(gen of serie.genero) {
+      if(gen === gene) {
+        //console.log(gene)
+        retorno.push(serie)
+      }
+    }
+  })
+  return retorno
+}
+console.log('queroGenero Caos:',queroGenero(series, "Caos"))
 
-
+function queroTitulo(series, parte){
+  var retorno = []
+  series.forEach(function(serie){
+      if(serie.titulo.indexOf(parte) !== -1) {
+        //console.log(gene)
+        retorno.push(serie)
+    }
+  })
+  return retorno
+}
+console.log('queroTitulo The:',queroTitulo(series, "The"))
 
 /*
-Exercicio 7
-
-Creditos
-
-Ao final de um episódio, temos os créditos do episódio. Para isso vamos implementar uma função, chamada de creditosIlluminatis Recebendo uma serie como parâmetro e imprima os créditos a partir dela. Deverá ser impresso, o Titulo da serie, os Diretores, avisando com um título que é o bloco deles; Em seguida vem o elenco, também com um título de Elenco;
-
-Tranquilo né? Easy! MAS, tem o seguinte: Os créditos são sempre ordenados alfabéticamente, mas pelo ULTIMO NOME!! Faça os ajustes necessários para que isso seja possível
-
+Exercicio 7: Creditos
+Ao final de um episódio, temos os créditos do episódio. Para isso vamos implementar uma função,
+ chamada de creditosIlluminatis Recebendo uma serie como parâmetro e imprima os créditos a partir dela.
+  Deverá ser impresso, o Titulo da serie, os Diretores, avisando com um título que é o bloco deles;
+  Em seguida vem o elenco, também com um título de Elenco;
+Tranquilo né? Easy! MAS, tem o seguinte: Os créditos são sempre ordenados alfabéticamente,
+ mas pelo ULTIMO NOME!! Faça os ajustes necessários para que isso seja possível
 Consulte as interwebsss para ajudar
+*/
 
-Exercicio 8
+console.log("===EXERCICIO 7===")
+function creditosIlluminatis(serie){
+  var stringao = ""
+  stringao = stringao.concat(serie.titulo, "\nDiretores\n")
+  var arrayDiretor =[]
+  for(diretores of serie.diretor) {
+    arrayDiretor.push(diretores)
+  }
+  ordenarDecrescente(arrayDiretor);
+  for(diretores of arrayDiretor) {
+    stringao = stringao.concat(diretores, ", ")
+  }
+  stringao = stringao.concat('\nElenco\n')
+  var arrayAtor = []
+  for(atores of serie.elenco) {
+    arrayAtor.push(atores)
+  }
+  ordenarDecrescente(arrayAtor)
+  for(atores of arrayAtor) {
+    stringao = stringao.concat(atores, ", ")
+  }
+  return stringao
+}
 
-Serie Illuminati
+function ordenarDecrescente(lista) {
+  lista.sort(function(a,b){
+    if(a < b) {
+      return 1
+    } else{
+      return -1
+    }
+  })
+}
 
+console.log(creditosIlluminatis(series[1]))
+
+/*
+Exercicio 8: Serie Illuminati
 Escondemos um pequeno easter egg neste exercicio!
-
 Para que ele seja descoberto, será necessário algumas informações;
-
 Quando abreviamos um nome colocamos a primeira letra do nome seguida de um ponto final Exemplo:
-
 Bernardo Bosak Rezende -> Bernardo B. Rezende
 Augusto Schiller Wartchow -> Augusto S. Wartchow
 Essa é a informação básica; Construa uma função que identificará aquela série que tem TODOS do elenco com nomes abreviados.
-
 Dica: Contrua uma função separada para identificar se aquela string tem a abreviação;
-
 Show de bola, estamos quase lá!
-
 Uma vez achada a serie, vamos modificar um pouquinho a implementação. Coloque todas as palavras abreviadas (de preferência sem os pontos finais) em uma string que será retornada ao final do método.
-
 Forme uma hashtag com a palavra! #PALAVRA
-
 Ao commitar, coloque no comentario do commit o que achou ;)
-
 Um bom FDS!
-
 NÃO SURTEM!!
 */
+
+console.log("===EXERCICIO 8===")
+
+function todosAbreviados(series) {
+  var detectados = 0
+  var elencoEspecial
+  series.forEach(function(serie){
+    for(ator of serie.elenco) {
+      if(detectaAbreviacoes(ator)){
+        detectados++
+        if(serie.elenco.length === detectados) {
+          elencoEspecial = serie.elenco
+          return
+        }
+      }
+    }
+    detectados = 0
+  })
+  return elencoEspecial
+}
+function detectaAbreviacoes(ator) {
+  if(ator.indexOf('.') !== -1) {
+    return true
+  }
+  return false
+}
+
+var elencoEA = todosAbreviados(series)
+
+function easterEgg(elenco) {
+  var stringao = ""
+  var stringao2 = ""
+  for(nome of elenco) {
+      stringao2 = stringao2.concat(nome,'')
+  }
+  arrayZao = stringao2.split(' ')
+  for(var i = 0; i < arrayZao.length; i++) {
+    if(arrayZao[i].indexOf('.') === -1){
+      arrayZao.splice(i, 1)
+      i = i-1
+      stringao = stringao.concat(arrayZao[i+1])
+    }
+  }
+  stringao = arrayZao.join('')
+  arrayZao = stringao.split('.')
+  stringao = '#'
+  stringao = stringao.concat(arrayZao.join(''))
+  return stringao
+}
+
+console.log(easterEggIlluminati = easterEgg(todosAbreviados(series)))
