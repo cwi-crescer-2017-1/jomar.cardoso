70d5209bd5f9d503a987ef5c9d2d9618a37aa487
Jomar <jomar.cardoso@cwi.com.br>
2017-05-20 04:59:27 -0300

    refactor(modulo5/aula3/atividade1)

--- modulo-05/aula03/atividade1/js/js.js
+++ modulo-05/aula03/atividade1/js/js.js
@@ -1,13 +1,16 @@
 var aula = angular.module('aula', []);
 
+aula.filter('trueFalse', () => nome => nome ? 'Sim' : 'Nao')
+
 aula.controller('novos-instrutores', function($scope){
   $scope.instrutores = instrutores;
   $scope.aulas = aulas;
-
-
-  $scope.incluir = () => {
-    console.log($scope.novoInstrutor)
-    $scope.instrutores.push($scope.novoInstrutor)
+  $scope.incluir = (novoInstrutor) => {
+    if($scope.formInstrutores.$invalid){
+      return
+    }
+    $scope.instrutores.push(angular.copy(novoInstrutor))
+    $scope.novoInstrutor = {}
   }
 });
 
@@ -17,7 +20,7 @@ let instrutores = [
     sobrenome: 'Rezende',
     idade: 30,
     email: 'bernardo@cwi.com.br',
-    jaDeuAula: true,
+    jaDeuAula: false,
     aula: 'OO'
   }
 ];
