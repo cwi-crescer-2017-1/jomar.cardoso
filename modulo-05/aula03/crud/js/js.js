67a0245265a4d537c0be72fb87bac2870156a7a2
Jomar <jomar.cardoso@cwi.com.br>
2017-05-21 01:13:23 -0300

    feat(crud): listar instrutores

--- modulo-05/aula03/crud/js/js.js
+++ modulo-05/aula03/crud/js/js.js
@@ -6,6 +6,15 @@ aula.filter('lpad2', () => numero => numero.toString().padStart(2, '0'))
 
 aula.controller('controller-aulas', function($scope){
   $scope.aulas = aulas
+  $scope.aulaAlterada = {
+    id: null,
+    nome: "",
+  }
+  $scope.armazenaid = function(id) {
+    console.log(id)
+    $scope.aulaAlterada.id = id
+  }
+
   $scope.incluirAula = function (novaAula) {
     if($scope.formNovaAula.$invalid){
       return
@@ -14,17 +23,30 @@ aula.controller('controller-aulas', function($scope){
     $scope.aulas.push(angular.copy(novaAula))
     $scope.novaAula = {}
   }
+
+
+
   $scope.alterarAula = function (aulaAlterada) {
     if($scope.formAlterarAula.$invalid){
+      console.log('form invalido')
+      return
+    }
+    if(aulaAlterada.id === null) {
+      aulaAlterada.id = $scope.aulas.length
+      $scope.aulas.push(angular.copy(aulaAlterada))
+      $scope.aulaAlterada = {}
+      for(aula of $scope.aulas) {
+        if(aula.id === aulaAlterada.id){
+          aula.nome = aulaAlterada.nome
+        }
+      }
       return
     }
     // console.log($scope.aulas.filter( a => a.id === aulaAlterada.id))
     // objeto = $scope.aulas.filter( a => a.id === aulaAlterada.id)
-    for(aula of $scope.aulas) {
-      if(aula.id === aulaAlterada.id){
-        aula.nome = aulaAlterada.nome
-      }
-    }
+    $scope.aulas[aulaAlterada.id].nome = aulaAlterada.nome
+    $scope.aulaAlterada = {}
+
   }
   $scope.excluirAula = function (aulaExcluida) {
     if($scope.formExcluirAula.$invalid){
@@ -39,14 +61,39 @@ aula.controller('controller-aulas', function($scope){
   }
 });
 
-aula.controller('controller-funcionarios', function($scope){
+aula.controller('controller-instrutores', function($scope){
   $scope.instrutores = instrutores;
+  $scope.aulas = aulas
+
+  $scope.incluirInstrutor = function (novoInstrutor) {
+    if($scope.formNovoInstrutor.$invalid){
+      return
+    }
+    novoInstrutor.id = $scope.instrutores.length
+    $scope.instrutores.push(angular.copy(novoInstrutor))
+    $scope.novoInstrutor = {}
+    
+  }
+  $scope.alterarAula = function (aulaAlterada) {
+    if($scope.formAlterarAula.$invalid){
+      return
+    }
+    // console.log($scope.aulas.filter( a => a.id === aulaAlterada.id))
+    // objeto = $scope.aulas.filter( a => a.id === aulaAlterada.id)
+    for(aula of $scope.aulas) {
+      if(aula.id === aulaAlterada.id){
+        aula.nome = aulaAlterada.nome
+      }
+    }
+  }
 })
 
 let aulas = [
   {id: 0, nome: 'Orientação a objetos'},
   {id: 1, nome: 'Banco de Dados'},
-  {id: 2, nome: 'HTML/CSS'}
+  {id: 2, nome: 'HTML/CSS'},
+  {id: 3, nome: 'Javascript'},
+  {id: 4, nome: 'AngularJS'}
 ];
 
 let instrutores = [
@@ -56,8 +103,38 @@ let instrutores = [
     sobrenome: 'Rezende',
     idade: 29,
     email: 'bernardo@cwi.com.br',
-    dandoAula: true,
-    aula: [aulas[0], aulas[1]],
+    lecionando: true,
+    aula: [aulas[0], aulas[3]],
     urlFoto: 'https://avatars0.githubusercontent.com/u/526075?v=3&s=460'
+  },
+  {
+    id: 1,
+    nome: 'André',
+    sobrenome: 'Nunes',
+    idade: 31,
+    email: 'nunes@cwi.com.br',
+    lecionando: false,
+    aula: [aulas[1]],
+    urlFoto: 'https://avatars2.githubusercontent.com/u/10319453?v=3&s=460'
+  },
+  {
+    id: 2,
+    nome: 'Pedro Henrique',
+    sobrenome: 'Pires',
+    idade: 23,
+    email: 'php@cwi.com.br',
+    lecionando: false,
+    aula: [aulas[2]],
+    urlFoto: 'https://avatars0.githubusercontent.com/u/6934800?v=3&s=460'
+  },
+  {
+    id: 3,
+    nome: 'Everton',
+    sobrenome: 'Zanatta',
+    idade: 25,
+    email: 'zanatta@cwi.com.br',
+    lecionando: true,
+    aula: [aulas[4]],
+    urlFoto: 'https://avatars2.githubusercontent.com/u/4175351?v=3&s=460'
   }
 ];
