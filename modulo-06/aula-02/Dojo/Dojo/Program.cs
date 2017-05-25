0caa1ba6ad6811251d7b53a6c0d3263baa5e95e9
Jomar <jomar.cardoso@cwi.com.br>
2017-05-25 19:08:05 -0300

    feat(ExercíciosLinqLambda): 1, 2, 3, 4

---
+++ modulo-06/aula-02/Dojo/Dojo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fs = File.OpenRead(@"C:\test.csv"))
+            using (var reader = new StreamReader(fs))
+            {
+                List<string> listA = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+                    listA.Add(values[0]); 
+                }
+            }
+        }
+
+        public static void main()
+        {
+
+        }
+    }
+}
+
