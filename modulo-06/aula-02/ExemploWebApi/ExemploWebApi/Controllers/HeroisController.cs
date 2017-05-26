1957241c3e414fe303e363052fb9abad76955834
Jomar <jomar.cardoso@cwi.com.br>
2017-05-26 17:37:24 -0300

    feat(webApi): exemplo em aula

---
+++ modulo-06/aula-02/ExemploWebApi/ExemploWebApi/Controllers/HeroisController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ExemploWebApi.Models;
+
+namespace ExemploWebApi.Controllers
+{
+    public class HeroisController : ApiController
+    {
+        private static List<Heroi> Herois = new List<Heroi>();
+        public static int QuantidadeHerois = 0;
+        public object objetoLock = new object();
+
+        public List<Heroi> Get(int? id = null)
+        {
+            if (id == null)
+            {
+                return Herois;
+            }
+            return Herois.Where(x => x.Id.Equals(id)).ToList();
+        }
+        public IHttpActionResult Post(Heroi heroi)
+        {
+            if (heroi.Id == 0)
+            {
+                lock(objetoLock)
+                {
+                    QuantidadeHerois++;
+                    heroi.Id = QuantidadeHerois;
+                    Herois.Add(heroi);
+                    return Ok();
+                }
+
+        }
+            else
+            {
+                return BadRequest();
+    }
+}
+    }
+}
