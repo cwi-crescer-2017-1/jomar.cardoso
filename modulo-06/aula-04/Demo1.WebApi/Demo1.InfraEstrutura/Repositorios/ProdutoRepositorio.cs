273f51d8079f1601b17aa2f116a24534048d3225
Jomar <jomar.cardoso@cwi.com.br>
2017-05-29 17:30:35 -0300

    feat(demo1): preparativo para exercicios

---
+++ modulo-06/aula-04/Demo1.WebApi/Demo1.InfraEstrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,56 @@
+﻿using Demo1.Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.InfraEstrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+
+        private static string CONEXAO_STRING =
+           "Server=13.65.101.67; Database=aluno02db; User id=jomar.cardoso; Password=123456";
+
+        public void Criar(Produto produto)
+        {           
+            using (var conexaoDb = new SqlConnection(CONEXAO_STRING))
+            {     
+                using (var command = conexaoDb.CreateCommand())
+                {
+                    command.CommandText = "SELECT @@IDENTITY";
+                    produto.Id = (int)(decimal)command.ExecuteScalar();
+                }
+            }
+          }
+        
+        public List<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+
+            using (var conexao = new SqlConnection(CONEXAO_STRING))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT Id, Nome, Preco, Estoque FROM Produto";
+
+                    var dataReader = comando.ExecuteReader();
+                    while(dataReader.Read())
+                    {
+                        var produto = new Produto();
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+                        produtos.Add(produto);
+                    }
+                }
+            }
+            return produtos;
+        }      
+    }
+}
---
+++ modulo-06/aula-04/Demo1.WebApi/Demo1.WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,36 @@
+﻿using Demo1.Dominio.Models;
+using Demo1.InfraEstrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace Demo1.WebApi.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+
+        private ProdutoRepositorio produtosRepositorio = new ProdutoRepositorio();
+
+
+        public IHttpActionResult Post(Produto produto)
+        {
+            var mensagens = new List<string>();
+            if (!produto.Validar(out mensagens))
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+
+            produtosRepositorio.Criar(produto);
+            return Ok(produto);
+        }
+
+        public IHttpActionResult Get()
+        {
+            var produtos = produtosRepositorio.Listar();
+            return Ok(produtos);
+        }
+    }
+}
