b9d3d64c584f5f49449b9695a72d8f27f6f57844
Jomar <jomar.cardoso@cwi.com.br>
2017-06-01 01:50:11 -0300

    feat(aula6/Entity): rotas autor

--- modulo-06/aula-05/EditoraCrescer/EditoraCrescer.Api/Controllers/AutoresController.cs
+++ modulo-06/aula-05/EditoraCrescer/EditoraCrescer.Api/Controllers/AutoresController.cs
@@ -9,7 +9,7 @@ using System.Web.Http;
 
 namespace EditoraCrescer.Api.Controllers
 {
-    
+    [RoutePrefix("api/autores")]
     public class AutoresController : ApiController
     {
         private AutorRepositorio repositorio = new AutorRepositorio();
@@ -19,16 +19,49 @@ namespace EditoraCrescer.Api.Controllers
         {
             return Ok(repositorio.Obter());
         }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(repositorio.Obter(id));
+        }
+
+        [HttpGet]
+        [Route("livros/{id}")]
+        public IHttpActionResult GetLivros(int id)
+        {
+            return Ok(repositorio.ObterLivros(id));
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IHttpActionResult Put(int id, Autor autor)
+        {
+            var mensagens = new List<string>();
+            if (repositorio.validar(id, autor, out mensagens))
+            {
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+            }
+            repositorio.alterar(id, autor);
+            return Ok(autor);
+        }
+
+        [HttpPost]
         public IHttpActionResult Post(Autor autor)
         {
             repositorio.Incluir(autor);
             return Ok(autor);
         }
+
+        [HttpDelete]
+        [Route("{id}")]
         public IHttpActionResult Delete(int id)
         {
             repositorio.Excluir(id);
             return Ok();
         }
+
 
         protected override void Dispose(bool disposing)
         {
--- modulo-06/aula-05/EditoraCrescer/EditoraCrescer/Repositorios/AutorRepositorio.cs
+++ modulo-06/aula-05/EditoraCrescer/EditoraCrescer/Repositorios/AutorRepositorio.cs
@@ -10,16 +10,28 @@ namespace EditoraCrescer.Repositorios
     public class AutorRepositorio
     {
         private Contexto contexto = new Contexto();
-
         public List<Autor> Obter()
         {
             return contexto.Autor.ToList();
         }
-
+        public List<Autor> Obter(int id)
+        {
+            return contexto.Autor.Where(a => a.Id == id).ToList();
+        }
+        public List<Livro> ObterLivros(int id)
+        {   
+            return contexto.Livro.Where(l => l.IdAutor == id).ToList();
+        }
         public void Incluir(Autor autor)
         {
             contexto.Autor.Add(autor);
             contexto.SaveChanges();
+        }
+        public Autor alterar(int isbn, Autor autor)
+        {
+            contexto.Entry(autor).State = System.Data.Entity.EntityState.Modified;
+            contexto.SaveChanges();
+            return autor;
         }
         public void Excluir(int id)
         {
@@ -27,6 +39,18 @@ namespace EditoraCrescer.Repositorios
             contexto.Autor.Attach(autor);
             contexto.Autor.Remove(autor);
             contexto.SaveChanges();
+        }
+        public bool validar(int id, Autor autor, out List<string> mensagens)
+        {
+            mensagens = new List<string>();
+            //verificar id com id do autor
+            if (autor.Id != id)
+                mensagens.Add("id invalido");
+            //verificar se tem autor com esta id
+            if (contexto.Autor.Count(l => l.Id == id) == 0)
+                mensagens.Add("Não encontrado");
+            var teste = mensagens.Count() != 0;
+            return mensagens.Count() != 0;
         }
     }
 }
