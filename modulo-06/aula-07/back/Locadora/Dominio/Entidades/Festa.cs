08cd1d74a357046d16b6c87020c16e3358627270
Jomar <jomar.cardoso@cwi.com.br>
2017-06-06 01:11:28 -0300

    feat(Locadora): Criado Classes do Dominio

---
+++ modulo-06/aula-07/back/Locadora/Dominio/Entidades/Festa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public enum Festa
+     {   
+         INFANTIL = 0,
+         DEBUTANTE = 1,
+         CASAMENTO = 2,
+         FORMATURA = 3,
+         ADULTA = 4
+     }
+}
---
+++ modulo-06/aula-07/back/Locadora/Dominio/Entidades/Pedido.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int Id_Cliente { get; set; }
+        public Cliente Cliente { get; set; }
+        public int Id_Produto { get; set; }
+        public Produto Produto { get; set; }
+        public int? Id_Pacote { get; set; }
+        public Pacote Pacote { get; set; }
+        //public int? Id_Opcional { get; set; }
+        public ICollection<Opcional> Opcionais { get; set; }
+        public DateTime DataPedido { get; set; }
+        public DateTime DataEntregaPrevista { get; set; }
+        public DateTime? DataEntregaRealizada { get; set; }
+        public decimal Valor { get; set; }
+        public decimal ValorTotal { get; set; }
+    }
+}
