d32b5153efb607817323ba615b83c9e5ea11cfdc
Jomar <jomar.cardoso@cwi.com.br>
2017-06-07 04:30:37 -0300

    feat(Locadora): Controller de Clientes, problemas com gitkraken

---
+++ modulo-06/Locadora/back/Locadora/Locadora.WebApi/Controllers/ClientesController.cs
@@ -0,0 +1,81 @@
+﻿using Locadora.Api.App_Start;
+using Locadora.Api.Models;
+using Locadora.Dominio.Entidades;
+using Locadora.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Locadora.Api.Controllers
+{
+    [BasicAuthorization]
+    [RoutePrefix("api/clientes")]
+    public class ClientesController : ControllerBasica
+    {
+        readonly ClienteRepositorio clienteRepositorio;
+
+        public ClientesController()
+        {
+            clienteRepositorio = new ClienteRepositorio();
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Obter()
+        {
+            return ResponderOK(clienteRepositorio.Obter());
+        }
+
+        [HttpGet, Route("{id:int}")]
+        public HttpResponseMessage Obter(int id)
+        {
+            var cliente = clienteRepositorio.Obter(id);
+            if (cliente == null)
+                return ResponderErro("Cliente não encontrado.");
+
+            return ResponderOK(new { cliente.Nome, cliente.Cpf, cliente.DataNascimento, cliente.Endereco, cliente.Genero});
+        }
+
+        [HttpGet, Route("{cpf}")]
+        public HttpResponseMessage ObterCpf(string cpf)
+        {
+            var cliente = clienteRepositorio.ObterCpf(cpf);
+            if (cliente == null)
+                return ResponderErro("Cliente não encontrado.");
+
+            return ResponderOK(new { cliente.Nome, cliente.Cpf, cliente.DataNascimento, cliente.Endereco, cliente.Genero });
+        }
+
+        [HttpGet, Route("{id:int}/locacoes")]
+        public IHttpActionResult ObterLocacoes(int id)
+        {
+            return Ok(clienteRepositorio.ObterLocacoes(id));
+        }
+
+        [HttpPost, Route("registrar")]
+        public HttpResponseMessage Registrar([FromBody]RegistrarClienteModel model)
+        {
+            if (clienteRepositorio.ObterCpf(model.Cpf) == null)
+            {
+                var cliente = new Cliente(model.Nome, model.Endereco, model.Cpf, model.DataNascimento, model.Genero);
+                if (cliente.Validar())
+                {
+                    clienteRepositorio.Criar(cliente);
+                }
+                else
+                {
+                    return ResponderErro(cliente.Mensagens);
+                }
+            }
+            else
+            {
+                return ResponderErro("Cliente já cadastrado.");
+            }
+            return ResponderOK();
+        }
+    }
+}
+
+
