06b784aeedfd61deb897499f93a10a0b694bb542
Jomar <jomar.cardoso@cwi.com.br>
2017-06-08 02:12:55 -0300

    feat(Locadora): Front Incluir Cliente Retorno de OK

--- modulo-06/Locadora/front/cliente/clienteController.js
+++ modulo-06/Locadora/front/cliente/clienteController.js
@@ -1,11 +1,18 @@
 biblioteca.controller('clienteController', function($scope, authService, clienteService, $location){
+    $scope.novoCliente = {}
     $scope.cadastrar = cadastrar
     $scope.cliente = {}
+    $scope.sucesso = false;
 
     function cadastrar(cliente) {
+        $scope.sucesso = false;
         clienteService.cadastrar(cliente)
         .then(response => {
             console.log(response)
+            if(response.status == 200) {
+                $scope.sucesso = true;
+                $scope.novoCliente = {}
+            }
         })
     }
 })
