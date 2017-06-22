ee83f3becdcdb084cb412c62257c9f6e56165255
Jomar <jomar.cardoso@cwi.com.br>
2017-06-22 16:58:02 -0300

    feat(modulo8/aula3): Dao

---
+++ modulo-08/aula3/src/main/java/br/com/crescer/aula3/TestDao.java
@@ -0,0 +1,28 @@
+package br.com.crescer.aula3;
+ 
+
+ 
+/**
+ 
+ * @author carloshenrique
+ 
+ */
+ 
+public interface TestDao {
+ 
+
+ 
+    void insert();
+ 
+
+ 
+    void create();
+ 
+
+ 
+    void drop();
+ 
+
+ 
+}
+ 
