885891cf6f6ecb93202ca8605b26b7bc32c0de0e
Jomar <jomar.cardoso@cwi.com.br>
2017-06-27 17:31:47 -0300

    feat(modulo8/aula6): preparativos

---
+++ modulo-08/aula6/src/main/java/Hello.java
@@ -0,0 +1,29 @@
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jomar.cardoso
+ */
+@ManagedBean
+@ViewScoped
+public class Hello {
+
+    private String helloWorld = "Hello World!";
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
+}
