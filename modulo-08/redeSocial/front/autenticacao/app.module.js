bbc7bdf64d7d92549d2f13a0c840b5e34476e901
Jomar <jomar.cardoso@cwi.com.br>
2017-06-30 17:39:50 -0300

    feat(front): rotas

--- modulo-08/redeSocial/front/autenticacao/app.module.js
+++ modulo-08/redeSocial/front/autenticacao/app.module.js
@@ -5,14 +5,14 @@ angular.module('app').constant('authConfig', {
 
     // Obrigatória - URL da API que retorna o usuário
     //urlUsuario: 'http://10.99.3.24/AutDemo.WebApi/api/acessos/usuario',
-    urlUsuario: 'http://localhost:9090/api/usuario',
+    urlUsuario: 'http://localhost:3000/api/acessos/usuario',
 
     // Obrigatória - URL da aplicação que possui o formulário de login
     urlLogin: '/login',
 
     // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGIN com sucesso
-    urlPrivado: '/home',
+    urlPrivado: '/privado',
 
     // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGOUT
-    urlLogout: '/login'
+    urlLogout: '/home'
 });
