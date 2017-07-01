ad302c9125374bfe3214821c3a18f72b2bc19fe6
Jomar <jomar.cardoso@cwi.com.br>
2017-07-01 04:45:16 -0300

    fix(redesocial): cors :heavy_check_mark:

--- modulo-08/redeSocial/back/redeSocial/src/main/java/br/com/crescer/redesocial/controllers/UsuarioRest.java
+++ modulo-08/redeSocial/back/redeSocial/src/main/java/br/com/crescer/redesocial/controllers/UsuarioRest.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,9 +32,9 @@ public class UsuarioRest {
     UsuarioService usuarioService;
     
     @PostMapping()
-    public String post(@RequestBody Usuario usuario) {
+    public Usuario post(@RequestBody Usuario usuario) {
         usuarioService.post(usuario);
-        return "Ok";
+        return usuario;
     }
     
     @DeleteMapping(value = "/{id}")
@@ -54,7 +55,7 @@ public class UsuarioRest {
     }
     
     @GetMapping(value = "/login")
-    public Usuario login(@RequestBody Usuario usuario) {
-        return usuarioService.findByEmail(usuario.getEmail());
+    public Usuario login(@RequestParam String email) {
+        return usuarioService.findByEmail(email);
     }
 }
--- modulo-08/redeSocial/back/redeSocial/src/main/java/br/com/crescer/redesocial/security/SocialWebSecurityConfig.java
+++ modulo-08/redeSocial/back/redeSocial/src/main/java/br/com/crescer/redesocial/security/SocialWebSecurityConfig.java
@@ -2,13 +2,18 @@ package br.com.crescer.redesocial.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import static org.springframework.http.HttpMethod.POST;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author carloshenrique
@@ -37,6 +42,21 @@ public class SocialWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .cors()
                 .and()
                 .csrf().disable();
+    }
+    
+    @Override
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring().antMatchers(securityPublic);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 
     @Autowired
